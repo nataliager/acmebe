a08ce417973b45079dcb2a3d1be43fb6d30f6394
nataliager <nathalia2001ge09@gmail.com>
2022-11-25 17:09:08 -0500

    registrar vistas

--- acmeApp/views/__init__.py
+++ acmeApp/views/__init__.py
@@ -1,1 +1,27 @@
+#Vistas modelo Usuario
+from .userCreateView import UserCreateView
+from .usersDetailView import UsersDetailView
+from .userDetailView import UserDetailView, LogUserDetailView, UserUpdateView
+from .login import Login
+
+#Vistas modelo Sede
+from .sedeCreateView import SedeCreateView
+from .sedesDetailView import SedeDetailView
+from .sedesDetailView import SedesDetailView
+
+#Vistas modelo Proveedor
+from .proveedorCreateView import ProveedorCreateView
+from .proveedoresDetailView import ProveedorDetailView
+from .proveedoresDetailView import ProveedoresDetailView
+
+#Vistas modelo Producto
+from .productoCreateView import ProductoCreateView
+from .productosDetailView import ProductoDetailView
+from .productosDetailView import ProductosDetailView
+
+#Vistas modelo Productoxsede
+from .productoxsedeCreateView import ProductoxsedeCreateView
+from .productoxsedeDetailView import ProductoxsedeDetailView
+from .productoxsedeDetailView import ProductosxsedeDetailView
+from .productoxsedeDetailView import  ProductoxsedeView
 
